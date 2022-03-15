010e5e9f70ce6edc8731aa1e43c9d393caa9fc25
Viktor <vikmillion@gmail.com>
2022-03-15 16:37:33 +0300

    fix code

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ document.querySelector(".press").addEventListener("click", () => {
     .then((json) => {
       renderTodos(json);
     });
-
-  const renderTodos = (todos) => {
-    console.log("todos :>> ", todos);
-    const outValue = document.getElementById("outValue");
-    const todo = document.createElement("div");
-    outValue.innerText = "";
-    if (todos.id > 0 || todos.id < 200) {
-      if (!todos.completed === false) {
-        delete todos.completed;
-        todos.state = "completed";
-      } else {
-        delete todos.completed;
-        todos.state = "in progress";
-      }
-    } else {
-      return (outValue.innerHTML = "Error<br/>Number 1 - 200  only");
-    }
-    console.log("entries 2:>> ", todos);
-
-    for (let key in todos) {
-      todo.innerHTML += " " + key + ": " + todos[key] + "<br/>";
-    }
-
-    outValue.append(todo);
-  };
 });
+
+const renderTodos = (todos) => {
+  console.log("todos :>> ", todos);
+  const outValue = document.getElementById("outValue");
+  const todo = document.createElement("div");
+  outValue.innerText = "";
+  if (todos.id > 0 || todos.id < 200) {
+    if (!todos.completed === false) {
+      delete todos.completed;
+      todos.state = "completed";
+    } else {
+      delete todos.completed;
+      todos.state = "in progress";
+    }
+  } else {
+    return (outValue.innerHTML = "Error<br/>Number 1 - 200  only");
+  }
+  console.log("entries 2:>> ", todos);
+
+  for (let key in todos) {
+    todo.innerHTML += `${key} : ${todos[key]}<br/>`;
+  }
+
+  outValue.append(todo);
+};
